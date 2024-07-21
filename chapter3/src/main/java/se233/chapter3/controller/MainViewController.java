6640fea57b4b26eb623474143c159b8d8a76b4ff
MT <minthantko101@gmail.com>
2024-07-22 01:09:55 +0700

    ch3_dropFileFunctionsInit

--- chapter3/src/main/java/se233/chapter3/controller/MainViewController.java
+++ chapter3/src/main/java/se233/chapter3/controller/MainViewController.java
@@ -1,4 +1,50 @@
 package se233.chapter3.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
+import javafx.scene.layout.Region;
+
+import java.io.File;
+
 public class MainViewController {
+    @FXML
+    private Region dropRegion;
+    @FXML
+    private ProgressBar progressBar;
+    @FXML
+    private ListView listView;
+
+    @FXML
+    public void initialize(){
+        dropRegion.setOnDragOver(event->{
+            Dragboard db=event.getDragboard();
+            final boolean isAccepted=db.getFiles().get(0).getName().toLowerCase().endsWith(".pdf");
+            if (db.hasFiles()&& isAccepted){
+                event.acceptTransferModes(TransferMode.COPY);
+            }else{
+                event.consume();
+            }
+        });
+
+        dropRegion.setOnDragDropped(event->{
+            Dragboard db=event.getDragboard();
+            boolean success=false;
+            if(db.hasFiles()){
+                success=true;
+                String filePath;
+                int totalFiles=db.getFiles().size();
+                for(int i=0;i<totalFiles;i++){
+                    File file=db.getFiles().get(i);
+                    filePath=file.getAbsolutePath();
+                    System.out.println(filePath);
+                }
+                progressBar.setProgress(100);
+            }
+            event.setDropCompleted(success);
+            event.consume();
+        });
+    }
 }
