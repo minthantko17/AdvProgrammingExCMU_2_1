76106390ad1cd7076d616d6b5e8cd395260be9a1
MT <minthantko101@gmail.com>
2024-09-07 22:42:48 +0700

    snakeHeadBodyTail

--- chapter5_part2/src/main/java/se233/chapter5_part2/model/Snake.java
+++ chapter5_part2/src/main/java/se233/chapter5_part2/model/Snake.java
@@ -1,4 +1,39 @@
 package se233.chapter5_part2.model;
 
+import javafx.geometry.Point2D;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Snake {
+    private Direction direction;
+    private Point2D head;
+    private Point2D prev_tail;
+    private List<Point2D> body;
+
+    public Snake(Point2D position) {
+        direction = Direction.DOWN;
+        body = new ArrayList<>();
+        this.head = position;
+        this.body.add(this.head);
+    }
+
+    public void move() {
+        head = head.add(direction.current);
+        prev_tail = body.remove(body.size() - 1);
+        body.add(0, head);
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+
+    public Direction getDirection() {
+        return this.direction;
+    }
+
+    public Point2D getHead() {
+        return head;
+    }
 }
+
--- chapter5_part2/src/test/java/se233/chapter5part2/SnakeTest.java
+++ chapter5_part2/src/test/java/se233/chapter5part2/SnakeTest.java
@@ -1,4 +1,37 @@
 package se233.chapter5part2;
 
+import javafx.geometry.Point2D;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import se233.chapter5_part2.model.Direction;
+import se233.chapter5_part2.model.Snake;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SnakeTest {
+    private Snake snake;
+
+    @BeforeAll
+    public static void initJfxRuntime() {
+        javafx.application.Platform.startup(() -> {});
+    }
+
+    @BeforeEach
+    public void setup() {
+        snake = new Snake(new Point2D(0, 0));
+    }
+
+    @Test
+    public void initialPosition_shouldBe_atOrigin() {
+        assertEquals(snake.getHead(), new Point2D(0, 0));
+    }
+
+    @Test
+    public void move_afterInitialized_headShouldBeInDownwardDirection() {
+        snake.setDirection(Direction.DOWN);
+        snake.move();
+        assertEquals(snake.getHead(), new Point2D(0, 1));
+    }
 }
+
